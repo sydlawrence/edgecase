c8e06cbb5b268a3eaa711e4d341fff8bfa0f4a76
Syd Lawrence <sydlawrence@gmail.com>
2015-09-24 13:10:31 +0100

    ms text

--- print.js
+++ print.js
@@ -3,6 +3,25 @@ var exec = require('child_process').exec;
 var actualPrint = true;
 
 module.exports.print = function(str) {
+  str += '\n\n\nThanks for popping by!\nFind out more at http://modern.ie';
+  str += '\n\n\n';
+
+  str = '                 __'+
+'            ,-~¨^  ^¨-,           _,'+
+'           /          / ;^-._...,¨/'+
+'          /          / /         /'+
+'         /          / /         /'+
+'        /          / /         /'+
+'       /,.-:''-,_ / /         /'+
+'       _,.-:--._ ^ ^:-._ __../'+
+'     /^         / /¨:.._¨__.;'+
+'    /          / /      ^  /'+
+'   /          / /         /'+
+'  /          / /         /'+
+' /_,.--:^-._/ /         /'+
+'^            ^¨¨-.___.:^ '+
+
+
   if (actualPrint) {
     var cmd = 'python print.py "' + str + '"';
     exec(cmd, function(error, stdout, stderr) {
