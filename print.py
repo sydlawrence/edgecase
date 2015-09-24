6c5ee77312e291856f2ca897296e70d03bb4c552
Syd Lawrence <sydlawrence@gmail.com>
2015-09-24 12:49:39 +0100

    added printer

---
+++ print.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import time
+import serial
+
+port = serial.Serial(port='/dev/ttyAMA0', baudrate=19200, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+
+port.open()
+port.flushOutput()
+
+port.write('hi there!\n');
+
+port.close();
