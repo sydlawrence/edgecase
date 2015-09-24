97af12ec89352b7e8d675e740d59620458d51875
Syd Lawrence <sydlawrence@gmail.com>
2015-09-24 12:18:25 +0100

    formatted responses

--- app.js
+++ app.js
@@ -15,15 +15,73 @@ stdin.setEncoding( 'UTF-8' );
 var waiting = false;
 var typed = '';
 
+var formatTestFail = function(test) {
+  var err = '';
+  switch(test.testName) {
+    case 'browserDetection':
+      err += 'You are checking user agents.\n';
+      for (var i in test.data) {
+        if (! test.data[i].passed) {
+          for (var j in test.data[i].data) {
+            if (! test.data[i].data[j].passed) {
+              if (i === 'comments') {
+                err += 'You are using browser specific comments on line ' + test.data[i].data.lineNumber + '\n';
+              }
+              else {
+                err += i + ': ' + test.data[i].data[j].url + ' line:' + test.data[i].data[j].lineNumber + ' ' + test.data[i].data[j].pattern + '\n';
+              }
+            }
+          }
+        }
+      }
+      break;
+    case 'cssprefixes':
+      err += 'You are not using standard css properties.\n';
+      for (var i in test.data) {
+        for (var j in test.data[i].selectors) {
+          err += test.data[i].cssFile + ' line:' +test.data[i].selectors[j].lineNumber + ' ' + test.data[i].selectors[j].selector + ': ' + test.data[i].selectors[j].styles.join(', ') + '\n';
+        }
+      }
+      break;
+    case 'jslibs':
+      err += 'You should keep your javascript libraries updated.\n';
+      for (var i in test.data) {
+        if (test.data[i].needsUpdate) {
+          err += test.data[i].name + ' is currently using version ' + test.data[i].version + ', you should update it to at least ' + test.data[i].minVersion + '\n';
+        }
+
+      }
+      break;
+    default:
+      err = JSON.stringify(test.data);
+  }
+  return err;
+  return test.data;
+};
+
 var sendTest = function() {
   console.log('RUNNING TESTS on ' + typed);
   var isHTTPS = false;
   api.test(typed, isHTTPS, function(d) {
-    console.log(d);
+    // console.log(d);
+    var iterator = 1;
+    for (var i in d.results) {
+      if (d.results[i].passed) {
+        process.stdout.write('\n\nTEST ' + iterator + ' PASSED (' + i + ')\n');
+      }
+      else {
+        process.stdout.write('\n\nTEST ' + iterator + ' FAILED (' + i + ')\n');
+        process.stdout.write(formatTestFail(d.results[i]));
+      }
+      iterator++;
+    }
     console.log('COMPLETED TESTS');
     typed = '';
   });
 };
+
+// typed = 'nfb.ca';
+// sendTest();
 
 
 var displayString = function() {
