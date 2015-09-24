73dfddd7523c6042b2cf0a117dccf7fbf165661a
Syd Lawrence <sydlawrence@gmail.com>
2015-09-24 13:15:45 +0100

    escapedg

--- print.js
+++ print.js
@@ -6,18 +6,19 @@ module.exports.print = function(str) {
   str += '\n\n\nThanks for popping by!\nFind out more at http://modern.ie';
   str += '\n\n\n';
   str = ''+
-'   _       _--""--_ \n'+
-'     " --""   |    |\n'+
-'   " . _|     |    |\n'+
-'   _    |  _--""--_|\n'+
-'     " --""   |    |\n'+
-'   " . _|     |    |\n'+
-'   _    |  _--""--_|\n'+
-'     " --""         \n';
+'   _       _--\"\"--_ \n'+
+'     \" --\"\"   |    |\n'+
+'   \" . _|     |    |\n'+
+'   _    |  _--\"\"--_|\n'+
+'     \" --\"\"   |    |\n'+
+'   \" . _|     |    |\n'+
+'   _    |  _--\"\"--_|\n'+
+'     \" --\"\"         \n';
 
 
   if (actualPrint) {
     var cmd = 'python print.py "' + str + '"';
+
     exec(cmd, function(error, stdout, stderr) {
       console.log(stdout);
     });
