ec0563954847c8194676871491a52725d50b4827
Syd Lawrence <sydlawrence@gmail.com>
2015-09-24 11:07:02 +0100

    now tests the url

---
+++ api.js
@@ -0,0 +1,20 @@
+var request = require('request');
+
+module.exports.query = function(url, isHTTPS, cb) {
+  var start = 'http://';
+  if (isHTTPS) {
+    start = 'https://';
+  }
+  var url = 'http://edge.azurewebsites.net/api/v2/scan?url=' + start + url;
+  request(url, cb);
+};
+
+module.exports.test = function(url, isHTTPS, cb) {
+  this.query(url, isHTTPS, function(error, response, body) {
+    body = JSON.parse(body);
+    cb(body);
+  });
+};
+
+
+
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var api = require('./api');
+
 var stdin = process.stdin;
 
 // without this, we would only get streams once enter is pressed
@@ -12,21 +14,42 @@ stdin.setEncoding( 'UTF-8' );
 
 var waiting = false;
 var typed = '';
+
+var sendTest = function() {
+  console.log('RUNNING TESTS on ' + typed);
+  var isHTTPS = false;
+  api.test(typed, isHTTPS, function(d) {
+    console.log(d);
+    console.log('COMPLETED TESTS');
+    typed = '';
+  });
+};
+
+
+var displayString = function() {
+  process.stdout.write( typed + '\n' );
+}
+
 // on any data into stdin
 stdin.on( 'data', function( key ){
+
+  if( key.charCodeAt(0) === 127 ){
+    typed = typed.substring(0, typed.length - 1);
+    displayString();
+    return;
+  }
+
   // ctrl-c ( end of text )
+  if (key === "\u000d") {
+    sendTest();
+    return;
+  }
   if ( key === '\u0003' ) {
     process.exit();
+    return;
   }
-  if (key.indexOf('P') > -1) {
-    waiting = false;
-    process.stdout.write('Alice, who the fuck is alice\n');
-  }
-  else {
-    typed += key;
-    waiting = true;
-    process.stdout.write( 'WAT: ' + typed + '\n' );
-  }
-  // write the key to stdout all normal like
-  // process.stdout.write( key );
+
+  typed += key;
+  displayString();
+
 });
