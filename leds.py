116ecdf025d8754ff39b165eecf16f3e48cac6dd
Paul King - Edge Case PI <paul@wemakeawesomesh.it>
2015-09-25 23:54:58 +0000

    Working led test

--- leds.py
+++ leds.py
@@ -1,8 +1,17 @@
 
 from Adafruit_MCP230xx import Adafruit_MCP230XX
+import time
 
 mcp = Adafruit_MCP230XX(busnum = 1, address = 0x20, num_gpios = 16)
-mcp.config(0, Adafruit_MCP230XX.OUTPUT)
-mcp.output(0, 1)  # Pin 0 High
 
+for i in range(0, 16):
+	mcp.config(i, Adafruit_MCP230XX.OUTPUT)
 
+for i in range(0, 16):
+	mcp.output(i, 0)  
+
+while True:
+	for i in [0, 1, 2, 8, 9, 10, 3, 4, 5, 11, 12, 13]:
+		mcp.output(i, 1)
+		time.sleep(0.1)
+		mcp.output(i, 0)
