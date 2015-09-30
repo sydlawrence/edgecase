6b428f46dfb35ab78620c5f84b3dd4ffe53bcf76
root <root@raspberrypi.(none)>
2015-09-30 14:10:30 +0000

    Dumb smoke routine

--- smoke.py
+++ smoke.py
@@ -9,9 +9,9 @@ smoke_addr = 0x08
 
 strength = ''
 
-for arg in sys.argv:
-  if arg != __file__:
-    strength = arg
+#for arg in sys.argv:
+# if arg != __file__:
+#   strength = arg
 
 print "puffing smoke"
-bus.write_byte(smoke_addr, strength)
+bus.write_byte(smoke_addr, 1)
