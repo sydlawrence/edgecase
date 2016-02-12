947df9b0d8bfb613f35678db458e1b0a5d4a5c35
thebeebs <martinbeeby@hotmail.com>
2016-02-12 18:51:58 +0000

    Added sounds

--- modules/sounds.js
+++ modules/sounds.js
@@ -7,6 +7,22 @@ module.exports.playVader = function() {
 	this.play('sounds/imperial_march.wav')
 };
 
+module.exports.playXP = function() {	
+	this.play('sounds/xp.wav')
+};
+
+module.exports.playCrystal = function() {	
+	this.play('sounds/crystal.wav')
+};
+
+module.exports.playDrWho = function() {	
+	this.play('sounds/drwho.wav')
+};
+
+module.exports.playHitchhickers = function() {	
+	this.play('sounds/hitchhickers.wav')
+};
+
 module.exports.play = function(file) {	
 	var cmd = 'sudo aplay ' + __dirname + '/' + file
 	exec(cmd, function() {    
